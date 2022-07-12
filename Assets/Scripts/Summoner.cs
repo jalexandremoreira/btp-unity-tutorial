ee4e857d45bae70d70542d3cc3d61fee23e0d7ca
Alexandre Moreira <brainmunchers@gmail.com>
2022-07-12 18:45:12 +0200

    add summoner code and variables

--- Assets/Scripts/Summoner.cs
+++ Assets/Scripts/Summoner.cs
@@ -14,7 +14,12 @@ public class Summoner : Enemy
 
     public float timeBetweenSummons;
     private float summonTime;
+    public float attackSpeed;
+    public float stopDistance;
+    private float timer;
 
+    public Enemy enemyToSummon;
+    
     // Start is called before the first frame update
     public override void Start()
     {
@@ -29,7 +34,7 @@ public class Summoner : Enemy
     private void Update()
     {
         if(player != null) {
-            if (Vector2.Distance(transform.position, targetPosition) > 0.5f) {
+            if(Vector2.Distance(transform.position, targetPosition) > 0.5f) {
                 transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
                 anim.SetBool("isRunning", true);
             } else {
@@ -39,6 +44,37 @@ public class Summoner : Enemy
                     anim.SetTrigger("summon");
                 }
             }
+
+            if(Vector2.Distance(transform.position, player.position) < stopDistance) {
+                if(Time.time > timer) {
+                    StartCoroutine(Attack());
+                    timer = Time.time + timeBetweenAttacks;
+                }
+            }
+        }
+    }
+
+    public void Summon(){
+        if(player != null) {
+            Instantiate(enemyToSummon, transform.position, transform.rotation);
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        player.GetComponent<Player>().TakeDamage(damage);
+
+        Vector2 originalPosition = transform.position;
+        Vector2 targetPosition = player.position;
+        
+        float percent = 0;
+        
+        while(percent <= 1) {
+            percent += Time.deltaTime * attackSpeed;
+            float formula = (-Mathf.Pow(percent, 2) + percent) * 4;
+            transform.position = Vector2.Lerp(originalPosition, targetPosition, formula);
+            
+            yield return null;
         }
     }
 }
